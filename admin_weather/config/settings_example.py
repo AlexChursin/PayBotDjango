f4fb8adcd67a72a24bc4ba2ff8b9fcbaa34812cd
Alexa_mobile <37036721+AlexaMobile@users.noreply.github.com>
2021-08-07 22:22:24 +0300

    Update settings_example.py

--- admin_weather/config/settings_example.py
+++ admin_weather/config/settings_example.py
@@ -62,23 +62,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'telegram_bot_2.db',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'DATABASE': 'bots',
+        'USER': 'bots_user',
+        'HOST': 'localhost',
+        'PASSWORD': 'MiP@ss1!'
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'DATABASE': 'bots',
-            'USER': 'bots_user',
-            'HOST': 'localhost',
-            'PASSWORD': 'MiP@ss1!'
-        }
-    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
